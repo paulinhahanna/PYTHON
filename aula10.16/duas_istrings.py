824b9aa177c79f3f3189df61b215d626de9d7fda
ana paula^C <paulinhaanna04gmail.com>
2023-10-17 16:32:04 -0300

    colocado  comentarios em duas istrings.py

---
+++ aula10.16/duas_istrings.py
@@ -0,0 +1,5 @@
+texto1 ="sou do" # texto1 recebe o valor " sou do"
+
+texto2 ="beck-end" #testo2 recebe o valor " beck-end"
+
+print(texto1,texto2) #a função print,concatena e imprime as duas variaveis na tela
