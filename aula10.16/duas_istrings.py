27e9a1c22993eba708f1a92e49688567324e0bd2
ana paula^C <paulinhaanna04gmail.com>
2023-10-17 16:46:34 -0300

    atualizado scapes do vscode

--- aula10.16/duas_istrings.py
+++ aula10.16/duas_istrings.py
@@ -1,5 +1,8 @@
 texto1 ="sou do" # texto1 recebe o valor " sou do"
 
-texto2 ="beck-end" #testo2 recebe o valor " beck-end"
+texto2 ="\"beck-end\""  #testo2 recebe o valor beck-end
 
 print(texto1,texto2) #a função print,concatena e imprime as duas variaveis na tela
+
+#carater SCAPE e (\) serve para salvar outros caracteres
+#pode usar aspas simples(')
