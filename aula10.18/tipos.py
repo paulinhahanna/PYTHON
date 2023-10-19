d791f27c0e06e7f4d18ab9cb777315a5cd891aee
ana paula^C <paulinhaanna04gmail.com>
2023-10-19 15:56:59 -0300

    strings concatenação e interpoalção estudadp

--- aula10.18/tipos.py
+++ aula10.18/tipos.py
@@ -3,7 +3,7 @@ b = 10.0   # float
 c = "10"  # string
 d = "10.0"  # string
 
-print (type(a)) suprime o tipo da variavel
+print (type(a)) # suprime o tipo da variavel
 print (type(b))
 print (type(c))
 print(type(d))
