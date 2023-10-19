a0acc67779b7c3b08f2b770ed03b6859df2f121e
ana paula^C <paulinhaanna04gmail.com>
2023-10-19 15:57:33 -0300

    estudos da aula doa dia 10/19

---
+++ aula10.18/aula10.19/tipos02.py
@@ -0,0 +1,12 @@
+a = 50
+b = "50"
+
+a = int(50)
+b = "50"
+
+print(type(a))
+print(type(b))
+
+# utilidade do float: moedas,peso,temperatura,medidas e outros casos parecidos
+idade = (input("qual sua idade?"))
+print(type(idade))
