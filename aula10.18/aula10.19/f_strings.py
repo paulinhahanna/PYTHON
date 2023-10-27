8c56a345f08136ad62f76b449b3d500acf17fbd3
ana paula^C <paulinhaanna04gmail.com>
2023-10-27 17:16:28 -0300

    foi deletado 4 arquivos e adicionado desafio 04.py

--- aula10.18/aula10.19/f_strings.py
+++
@@ -1,8 +0,0 @@
-# interpolando strings com codigo python
-nome = input("qual é seu nome?")
-
-#concatenação
-print("seja muito bem vindo",nome)
-
-# interpolação
-print (f'seja bem vindo {nome}. Estavamos esperando por você!!!')
--- aula10.18/desafio04.py
+++ aula10.18/desafio04.py
@@ -1,3 +1,9 @@
 #fazer 4 variveis pra receber 4 inputs
 
-
+variavel1 = input("nota1:")
+variavel2 = input("nota2:")
+variavel3 = input("nota3:")
+variavel4 = input("nota4:")
+variavel5 = 4
+soma = (str(variavel1) + (variavel2) + (variavel3) + (variavel4))
+print (float( soma) / int (variavel5))
