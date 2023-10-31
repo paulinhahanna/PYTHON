167e3e44b15f7768359bae1393e68deff449a9fe
ana paula^C <paulinhaanna04gmail.com>
2023-10-31 16:10:19 -0300

    desafio04,py concluido

--- aula10.18/desafio04.py
+++ aula10.18/desafio04.py
@@ -1,9 +1,10 @@
 #fazer 4 variveis pra receber 4 inputs
 
-variavel1 = input("nota1:")
-variavel2 = input("nota2:")
-variavel3 = input("nota3:")
-variavel4 = input("nota4:")
-variavel5 = 4
-soma = (str(variavel1) + (variavel2) + (variavel3) + (variavel4))
-print (float( soma) / int (variavel5))
+nota1 = input('nota1:')
+nota2 = input('nota2:')
+nota3 = input('nota3:')
+nota4 = input('nota4:')
+
+media =  (int(nota1) + int(nota2) + int(nota3) + int(nota4))  /  4 
+
+print("media:", media)
