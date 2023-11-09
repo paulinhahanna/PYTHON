4515847bada20931748aaa258590e9cf72263d59
ana paula^C <paulinhaanna04gmail.com>
2023-11-09 14:17:25 -0300

    atividade do dia 10/31 feita

---
+++ aula10.31/exercios.py
@@ -0,0 +1,11 @@
+distanciaEmKm = int(input('Digite a distancia em km'))
+qntddEmLt = int(input('Digite a distancia em km'))
+
+consumo = 
+
+if (consumo < 8):
+    print ("Venda o carro")
+elif (consumo >= 8 and consumo <= 14):
+    print ("Economico")
+else:
+    print ("Super economico")
